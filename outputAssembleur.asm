a8c96bd198d5b02fe2ecbab2a18b05fc219f0177
JulienGln <juliengalerne.jg@gmail.com>
2024-04-10 19:32:26 +0200

    Create outputAssembleur.asm

---
+++ outputAssembleur.asm
@@ -0,0 +1,19 @@
+DATA SEGMENT
+DATA ENDS
+CODE SEGMENT
+		mov eax, 200
+mov eax, prixHt
+			mov eax, prixHt
+mov eax, 119
+pop ebx
+pop eax
+mul eax, ebx
+push eax
+mov eax, 100
+pop ebx
+pop eax
+div ebx
+push eax
+mov eax, prixTtc
+
+CODE ENDS
