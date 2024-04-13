b819aad7761d15181c2bbe16bf33682fa30281b6
JulienGln <juliengalerne.jg@gmail.com>
2024-04-13 16:32:35 +0200

    exemple avec or

--- output.asm
+++ output.asm
@@ -1,27 +1,20 @@
 DATA SEGMENT
 	a DD
+	b DD
 DATA ENDS
 CODE SEGMENT
-	mov eax, 5
-	push eax
-	mov eax, 5
-	pop ebx
-	sub eax, ebx
-	jz vrai_and_2
-	mov eax, 0
-	jmp sortie_and_2
-vrai_and_2:
 	mov eax, 1
-sortie_and_2:
-	push eax
-	mov eax, 6
-	pop ebx
-	sub eax, ebx
-	jz vrai_and_2
-	mov eax, 0
-	jmp sortie_and_2
-vrai_and_2:
-	mov eax, 1
-sortie_and_2:
 	mov a, eax
+	mov eax, a
+	push eax
+	mov eax, 0
+	pop ebx
+	sub eax, ebx
+	jnz vrai_or_1
+	mov eax, 0
+	jmp sortie_or_1
+vrai_or_1:
+	mov eax, 1
+sortie_or_1:
+	mov b, eax
 CODE ENDS
